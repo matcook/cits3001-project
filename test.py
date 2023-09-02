85a06e34468d6cc3aba45a46b38be5427a5bf09b
matcook <mat.cook@bigpond.com>
2023-09-02 15:10:57 +0800

    Sleep delay in loop

--- test.py
+++ test.py
@@ -2,18 +2,20 @@ from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 import gym
+#import time
 
 env = gym.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode="human")
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 
-done = True
-env.reset()
-for step in range(5000):
+observation, info = env.reset()
+
+for step in range(10000):
     action = env.action_space.sample()
     obs, reward, terminated, truncated, info = env.step(action)
-    done = terminated or truncated
 
-    if done:
-       env.reset()
+    if terminated or truncated:
+       observation, info = env.reset()
+    
+    #time.sleep(0.02)
 
 env.close()
