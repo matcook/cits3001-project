405d57a815c42b2c5db3d6252c3450a8cd268b66
matcook <mat.cook@bigpond.com>
2023-09-22 12:52:29 +0800

    mario now jumps over pipes

--- rulebased.py
+++ rulebased.py
@@ -5,6 +5,8 @@ import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 import gym
 
+print(SIMPLE_MOVEMENT)
+
 # Load all templates
 mario_templates = [cv2.imread(f'templates/mario{i}.png', cv2.IMREAD_COLOR) for i in ['A', 'B', 'C', 'D', 'E', 'F', 'G']]
 blocks_templates = [cv2.imread(f'templates/block{i}.png', cv2.IMREAD_COLOR) for i in range(1, 5)]
@@ -16,23 +18,12 @@ question_templates = [cv2.imread(f'templates/question{i}.png', cv2.IMREAD_COLOR)
 tall_mario_templates = [cv2.imread(f'templates/tall_mario{i}.png', cv2.IMREAD_COLOR) for i in ['A', 'B', 'C']]
 goomba_template = cv2.imread('templates/goomba.png', cv2.IMREAD_COLOR)
 
-def draw_rectangles(image, locations, template_shape, color=(0, 255, 0)):
-    """Draw rectangles around all detected positions of an object."""
-    # Convert the image to a compatible type
-    image = np.ascontiguousarray(image, dtype=np.uint8)
-    
-    for (x, y) in locations:
-        bottom_right = (x + template_shape[1], y + template_shape[0])
-        cv2.rectangle(image, (x, y), bottom_right, color, 2)
-    
-    return image
-
 def detect_objects(observation_bgr, templates):
     best_locations = []
 
     for template in templates:
         res = cv2.matchTemplate(observation_bgr, template, cv2.TM_CCOEFF_NORMED)
-        threshold = 0.6
+        threshold = 0.46  
         loc = np.where(res >= threshold)
         
         if loc[0].size:
@@ -49,37 +40,19 @@ def detect_all_objects(observation):
         "blocks": detect_objects(observation_bgr, blocks_templates),
         "koopas": detect_objects(observation_bgr, koopas_templates),
         "mushroom": detect_objects(observation_bgr, [mushroom_template]),
-        "pipe": detect_objects(observation_bgr, [pipe_upper_template] + [pipe_lower_template]),
-        #"pipe_lower": detect_objects(observation_bgr, [pipe_lower_template]),
+        "pipe_upper": detect_objects(observation_bgr, [pipe_upper_template]),
+        "pipe_lower": detect_objects(observation_bgr, [pipe_lower_template]),
         "question": detect_objects(observation_bgr, question_templates)
     }
 
 def rule_based_action(observation):
-    # Make a copy of the observation to draw on
-    observation_copy = observation.copy()
-
     detected_objects = detect_all_objects(observation)
     mario_positions = detected_objects["mario"]
     goomba_positions = detected_objects["goomba"]
-    pipe_positions = detected_objects["pipe"]
-    question_positions = detected_objects["question"]
-    block_positions = detected_objects["blocks"]
-
-
-    # Drawing rectangles for debugging
-    draw_rectangles(observation_copy, mario_positions, mario_templates[0].shape, color=(0, 255, 0))
-    draw_rectangles(observation_copy, goomba_positions, goomba_template.shape, color=(0, 0, 255))
-    draw_rectangles(observation_copy, pipe_positions, goomba_template.shape, color=(255, 0, 0))
-    draw_rectangles(observation_copy, question_positions, goomba_template.shape, color=(255, 0, 255))
-    draw_rectangles(observation_copy, block_positions, goomba_template.shape, color=(255, 255, 0))
-    # ... (and repeat for other detected objects as necessary with different colors)
-    
-    # Show the modified observation with rectangles
-    cv2.imshow('Debugging Observation', cv2.cvtColor(observation_copy, cv2.COLOR_RGB2BGR))
-    cv2.waitKey(1)  # Display the image for a short duration
+    pipe_upper_positions = detected_objects["pipe_upper"]
 
     # Default action
-    action = 3  # corresponds to running right in SIMPLE_MOVEMENT
+    action = 1  # corresponds to running right in SIMPLE_MOVEMENT
 
     if mario_positions and goomba_positions:
         mario_central_x = mario_positions[0][0] + mario_templates[0].shape[1] // 2
@@ -89,6 +62,14 @@ def rule_based_action(observation):
                 action = 4  # corresponds to jumping right in SIMPLE_MOVEMENT
                 break
 
+    if mario_positions and pipe_upper_positions:
+        mario_central_x = mario_positions[0][0] + mario_templates[0].shape[1] // 2
+        for pipe_position in pipe_upper_positions:
+            distance = pipe_position[0] - mario_central_x
+            if 0 < distance <= 45:
+                action = 4  # corresponds to jumping right in SIMPLE_MOVEMENT
+                break
+    
     return action
 
 # Setup environment
@@ -99,6 +80,7 @@ observation, info = env.reset()
 
 for step in range(10000):
     action = rule_based_action(observation)
+    #print(action)
     obs, reward, terminated, truncated, info = env.step(action)
     if terminated or truncated:
         observation, info = env.reset()
